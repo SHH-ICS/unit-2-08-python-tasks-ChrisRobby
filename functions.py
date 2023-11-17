f03e3e4e0bddb2bd60352370d13e9c0b5cd9b5bb
ChrisRobby <144351829+ChrisRobby@users.noreply.github.com>
2023-11-16 22:49:36 -0500

    Fix functions .pi

--- functions.py
+++ functions.py
@@ -1,11 +1,17 @@
 # TASK: Update both functions to reverse the letters in the name and provide the square root of the users age.
 
+import math
+
 # Returns a string that is the contents of the myName variable written backwards
 # ie: passing Atkinson as the parameter would return nosniktA
 def reverseName(myName):
   # Write your code here
+  result = ""
+  length = len(myName)
+  while length > 0:
+    result = result + myName[length - 1]
+    length = length - 1
   
-  result = myName
   return result
 
 # Returns a float value that is the contents of the myAge variable square rooted
@@ -13,5 +19,5 @@ def reverseName(myName):
 def rootAge(myAge):
   # Wrie your code here
   
-  result = myAge
+  result = math.sqrt(int(myAge))
   return result
